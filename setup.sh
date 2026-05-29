a64b1080bf9074ffed1794f3707f4bf78f7b2bbf
Reggie McMurtrey <reggie.mcmurtrey@gmail.com>
2026-05-29 16:11:37 +0000

    fixes to setup.sh

--- setup.sh
+++ setup.sh
@@ -4,19 +4,26 @@ sudo apt install build-essential git uuid-dev iasl nasm python3 python-is-python
 
 #clone edk2
 git clone https://github.com/tianocore/edk2.git
+cd edk2
+git submodule update --init --recursive
+git checkout edk2-stable202605
+cd ..
 
 #clone edk2-libc
 git clone https://github.com/tianocore/edk2-libc.git
+cd edk2-libc
+git checkout v3.6.8.2
+cd ..
 
 #symlinks from edk2-libc to edk2
 cd edk2
+ln -s ../edk2-libc/AppPkg AppPkg
 ln -s ../edk2-libc/StdLib StdLib
 ln -s ../edk2-libc/StdLibPrivateInternalFiles StdLibPrivateInternalFiles
 cd ..
 
 #clone pciutils.efi
-#FIXME: This need to point to my fork
-git clone https://github.com/timotheuslin/pciutils.efi.git
+git clone https://github.com/reggie-mcmurtrey/pciutils.efi.git
 
 #symlinks from pciutils.efi to edk2
 cd edk2
@@ -25,14 +32,19 @@ cd ..
 
 #clone pciutils
 git clone https://github.com/pciutils/pciutils.git
+cd pciutils
+git checkout v3.15.0
+cd ..
 
 #symlinks from pciutils to pciutils.efi
-cd pciutils.efi/PciUtilsPkg
+cd pciutils.efi
+cd PciUtilsPkg
 ln -s ../../pciutils pciutils
+cd ..
 cd ..
 
 #build everything
 cd edk2
-make -C BaseTools
 source edksetup.sh
+make -C BaseTools
 build -p PciUtilsPkg/PciUtilsPkg.dsc -a X64 -t GCC -b RELEASE
